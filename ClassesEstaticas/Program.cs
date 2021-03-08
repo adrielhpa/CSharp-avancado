a81cfd6af2965e013d6c9cd7b09306882ef37900
Adriel <adriel_hp.andrade@outlook.com>
2021-03-07 21:17:01 -0300

    membros estaticos

---
+++ ClassesEstaticas/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ClassesEstaticas
+{
+
+    public static class ConversorStatic
+    {
+        public static double CelsiusToFah(double temperatura)
+        {
+            return (temperatura * 9 / 5) + 32;
+        }
+        public static double FahToCelsius(double temperatura)
+        {
+            return (temperatura -32)* 5 / 9;
+        }
+    }
+
+    public class ConversorInstancia
+    {
+        public static double temperatura;
+        public double CelsiusToFah()
+        {
+            return (temperatura * 9 / 5) + 32;
+        }
+        public double FahToCelsius()
+        {
+            return (temperatura -32)* 5 / 9;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c1 = new ConversorInstancia();
+            var c2 = new ConversorInstancia();
+            ConversorInstancia.temperatura = 30;            
+            var celsius = c1.CelsiusToFah();
+            ConversorInstancia.temperatura = 70;
+            var fah = c1.FahToCelsius();
+            System.Console.WriteLine("Celsius = " + celsius);
+            System.Console.WriteLine("Fah = " + fah);
+            Console.ReadLine();
+        }
+
+        private static void TesteClasseEstatica()
+        {
+            var temperatura = 35.0;
+            temperatura = ConversorStatic.CelsiusToFah(temperatura);
+            System.Console.WriteLine(temperatura);
+        }
+    }   
+}
---
+++ TypeInference_Dynamic/TypeInference_Dynamic/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeInference_Dynamic
+{
+    public class Connection
+    {
+        public string ConnectionString;
+        public void Conectar()
+        {
+            Console.WriteLine("Conectando...");
+        }
+        public void Fechar()
+        {
+            Console.WriteLine("Fechando...");
+        }
+    }
+
+    public static class Fabrica
+    {
+        public static Connection CriarConnection()
+        {
+            return new Connection();
+        }
+    }
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            var str = "TESTE";
+            var x = 10;
+            dynamic v = "TESTE";
+            var con = Fabrica.CriarConnection();
+            con.ConnectionString = "BANCO";
+            con.Conectar();
+            con.Fechar();
+
+            Console.ReadLine();
+        }
+    }
+}
