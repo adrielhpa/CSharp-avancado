6cb86ac60a9a83b66ba622596ac27bebdedd4385
Adriel <adriel_hp.andrade@outlook.com>
2021-03-09 18:58:49 -0300

    Classes de Extensão

---
+++ ClassesExtensao/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data;
+
+namespace ClassesExtensao
+{
+    public static class MyExtensions
+    {
+        public static void WriteJSON(this DataSet ds, string filaName)
+        {
+            //...
+        }
+
+        public static string ToUrl(this string str)
+        {
+            return str = "http://" + str;
+        }
+    }    
+
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            // var ds = new DataSet();
+            // ds.WriteJSON("dados.js");
+            
+            // ds.WriteXml("dados.xml");
+
+            string url = "www.linkedin.com/in/adriel-andrade-78024a163/";
+            System.Console.WriteLine(url.ToUrl());
+            Console.ReadLine();
+
+            
+        }
+    }
+}
